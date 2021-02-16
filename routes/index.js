8a83e33c0d96d65cdfd423d97d5f54531fc8c9e7
Nicole Martindale <nmartind@ucsd.edu>
2021-02-15 19:24:07 -0800

    added node.js functionality

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+/*
+ * GET home page.
+ */
+
+exports.view = function(req, res){
+    res.render('index');
+};
+
+exports.newrecipe = function(req, res){
+    res.render('newrecipe');
+};
+
+exports.savedrecipes = function(req, res){
+    res.render('savedrecipes');
+};

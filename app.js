814a147467fab9f7526f3411ab6cc5b19832d02f
Nicole Martindale <nmartind@ucsd.edu>
2021-02-15 20:13:05 -0800

    does this work

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ app.get('/savedrecipes', index.savedrecipes);
 // Example route
 // app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });

e9c1c06ea63538194cc430503963285f8f74a08f
Nicole Martindale <nmartind@ucsd.edu>
2021-02-23 18:34:29 -0800

    chrome options

--- recipescript.py
+++ recipescript.py
@@ -9,8 +9,17 @@ import sys, time, os
 # assert "No results found." not in driver.page_source
 # driver.close()
 
+# os.environ["GOOGLE_CHROME_BIN"] = "/usr/bin/google-chrome"
+# os.environ["CHROMEDRIVER_PATH"] = "/usr/local/bin/chromedriver"
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
 def findRecipe(dish, optional):
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     dish = dish.replace(" ", "%20")
     if optional != "none":
@@ -52,7 +61,7 @@ def findRecipe(dish, optional):
     return ingredients, instructions, orig_serving
 
 def getCorrectServing(ingredients, orig_serving, serving):
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get("https://mykitchencalculator.com/recipeconverter.html")
     # clear = driver.find_element_by_xpath("//*[@id='clearListButton']")
     # clear.click()
@@ -86,13 +95,6 @@ def getCorrectServing(ingredients, orig_serving, serving):
 
 #start process
 if __name__ == '__main__':
-
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     dish = str(sys.argv[1])
     serving = str(sys.argv[2])
